77c4dc4aea426154c05ed9446fc16f5f619ef53f
Cris <cristina.picatoste@gmail.com>
2021-04-01 18:03:28 +0200

    cart reducer, updated collection item

--- src/components/CollectionPreview/CollectionPreview.js
+++ src/components/CollectionPreview/CollectionPreview.js
@@ -11,8 +11,10 @@ const CollectionPreview = ({title, items}) => {
       <div className="preview">
         {items
         .filter((item, idx) => idx < 4)
-        .map(({id, ...item}) => (
-          <CollectionItem key={id} {...item} />
+        .map(item => (
+          <CollectionItem key={item.id} item={item} />
+        // .map(({id, ...item}) => (
+        //   <CollectionItem key={id} {...item} />
           // <div key={item.id} >{item.name}</div>
         ))}
       </div>
